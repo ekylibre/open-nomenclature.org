3e61f6ce640a138ac56f2d0adf9a4712b8039e99
Brice TEXIER <brice@ekylibre.com>
2015-07-02 21:47:04 +0200

    Bumps to 0.0.1

--- lib/nomen/version.rb
+++ lib/nomen/version.rb
@@ -2,7 +2,7 @@ module Nomen
   module Version
     MAJOR = '0'
     MINOR = '0'
-    PATCH = '0'
+    PATCH = '1'
   end
   VERSION = [Version::MAJOR, Version::MINOR, Version::PATCH].compact.join('.').freeze
 end
